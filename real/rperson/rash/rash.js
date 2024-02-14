23f911eb2260369878c6a4f8f7f9570f62a8033e
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:35:07 -0500

    Update rash.js

--- real/rperson/rash/rash.js
+++ real/rperson/rash/rash.js
@@ -2,11 +2,11 @@ function one() {
     document.getElementById('picture').src='../../rpictures/pic5.JPEG';
 }
 function two() {
-    document.getElementById('picture').src='../../rpictures/pic6.JPeG';
+    document.getElementById('picture').src='../../rpictures/pic6.JPEG';
 }
 function three() {
-    document.getElementById('picture').src='../../rpictures/pic7.jpeg';
+    document.getElementById('picture').src='../../rpictures/pic7.JPEG';
 }
 function four() {
-    document.getElementById('picture').src='../../rpictures/pic32.jpeg'; 
+    document.getElementById('picture').src='../../rpictures/pic32.JPEG'; 
 }
