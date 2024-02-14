9d7d7aad78d4f23f0daaadb80c34778b6cf64a1a
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:16:57 -0500

    Add files via upload

---
+++ virtual/vperson/vsyd/vsyd.js
@@ -0,0 +1,18 @@
+function one() {
+    document.getElementById('picture').src='../../vpictures/pic3.png';
+}
+function two() {
+    document.getElementById('picture').src='../../vpictures/pic4.png';
+}
+function three() {
+    document.getElementById('picture').src='../../vpictures/pic7.png';
+}
+function four() {
+    document.getElementById('picture').src='../../vpictures/pic8.png'; 
+}
+function five() {
+    document.getElementById('picture').src='../../vpictures/pic9.png';
+}
+function six() {
+    document.getElementById('picture').src='../../vpictures/pic27.png';
+}
