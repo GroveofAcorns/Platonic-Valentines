ef8292f48afb869e6b55671c1b2fd509e65d741c
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:39:30 -0500

    Update rall.js

--- real/rall.js
+++ real/rall.js
@@ -1,96 +1,96 @@
 function one() {
-    document.getElementById('picture').src='rpictures/pic1.jpg';
+    document.getElementById('picture').src='rpictures/pic1.JPG';
 }
 function two() {
-    document.getElementById('picture').src='rpictures/pic2.jpg';
+    document.getElementById('picture').src='rpictures/pic2.JPG';
 }
 function three() {
-    document.getElementById('picture').src='rpictures/pic3.jpeg';
+    document.getElementById('picture').src='rpictures/pic3.JPEG';
 }
 function four() {
-    document.getElementById('picture').src='rpictures/pic4.jpeg'; 
+    document.getElementById('picture').src='rpictures/pic4.JPEG'; 
 }
 function five() {
-    document.getElementById('picture').src='rpictures/pic5.jpeg';
+    document.getElementById('picture').src='rpictures/pic5.JPEG';
 }
 function six() {
-    document.getElementById('picture').src='rpictures/pic6.jpeg';
+    document.getElementById('picture').src='rpictures/pic6.JPEG';
 }
 function seven() {
-    document.getElementById('picture').src='rpictures/pic7.jpeg';
+    document.getElementById('picture').src='rpictures/pic7.JPEG';
 }
 function eight() {
-    document.getElementById('picture').src='rpictures/pic8.jpeg'; 
+    document.getElementById('picture').src='rpictures/pic8.JPEG'; 
 }
 function nine() {
-    document.getElementById('picture').src='rpictures/pic9.jpg'; 
+    document.getElementById('picture').src='rpictures/pic9.JPG'; 
 }
 function one0() {
-    document.getElementById('picture').src='rpictures/pic10.jpeg';
+    document.getElementById('picture').src='rpictures/pic10.jJPEG';
 }
 function one1() {
-    document.getElementById('picture').src='rpictures/pic11.jpeg';
+    document.getElementById('picture').src='rpictures/pic11.JPEG';
 }
 function one2() {
-    document.getElementById('picture').src='rpictures/pic12.jpeg';
+    document.getElementById('picture').src='rpictures/pic12.JPEG';
 }
 function one3() {
-    document.getElementById('picture').src='rpictures/pic13.jpeg'; 
+    document.getElementById('picture').src='rpictures/pic13.JPEG'; 
 }
 function one4() {
-    document.getElementById('picture').src='rpictures/pic14.jpeg';
+    document.getElementById('picture').src='rpictures/pic14.JPEG';
 }
 function one5() {
-    document.getElementById('picture').src='rpictures/pic15.jpg';
+    document.getElementById('picture').src='rpictures/pic15.JPG';
 }
 function one6() {
-    document.getElementById('picture').src='rpictures/pic16.jpeg';
+    document.getElementById('picture').src='rpictures/pic16.JPEG';
 }
 function one7() {
-    document.getElementById('picture').src='rpictures/pic17.jpg'; 
+    document.getElementById('picture').src='rpictures/pic17.JPG'; 
 }
 function one8() {
-    document.getElementById('picture').src='rpictures/pic18.jpg'; 
+    document.getElementById('picture').src='rpictures/pic18.JPG'; 
 }
 function one9() {
-    document.getElementById('picture').src='rpictures/pic19.jpg';
+    document.getElementById('picture').src='rpictures/pic19.JPG';
 }
 function two0() {
-    document.getElementById('picture').src='rpictures/pic20.jpg';
+    document.getElementById('picture').src='rpictures/pic20.JPG';
 }
 function two1() {
-    document.getElementById('picture').src='rpictures/pic21.jpg';
+    document.getElementById('picture').src='rpictures/pic21.JPG';
 }
 function two2() {
-    document.getElementById('picture').src='rpictures/pic22.jpg'; 
+    document.getElementById('picture').src='rpictures/pic22.JPG'; 
 }
 function two3() {
-    document.getElementById('picture').src='rpictures/pic23.jpg';
+    document.getElementById('picture').src='rpictures/pic23.JPG';
 }
 function two4() {
-    document.getElementById('picture').src='rpictures/pic24.jpg';
+    document.getElementById('picture').src='rpictures/pic24.JPG';
 }
 function two5() {
-    document.getElementById('picture').src='rpictures/pic25.jpg';
+    document.getElementById('picture').src='rpictures/pic25.JPG';
 }
 function two6() {
-    document.getElementById('picture').src='rpictures/pic26.jpg'; 
+    document.getElementById('picture').src='rpictures/pic26.JPG'; 
 }
 function two7() {
-    document.getElementById('picture').src='rpictures/pic27.jpg'; 
+    document.getElementById('picture').src='rpictures/pic27.JPG'; 
 }
 function two8() {
-    document.getElementById('picture').src='rpictures/pic28.jpg';
+    document.getElementById('picture').src='rpictures/pic28.JPG';
 }
 function two9() {
-    document.getElementById('picture').src='rpictures/pic29.jpeg';
+    document.getElementById('picture').src='rpictures/pic29.JPEG';
 }
 function three0() {
-    document.getElementById('picture').src='rpictures/pic30.jpeg'; 
+    document.getElementById('picture').src='rpictures/pic30.JPEG'; 
 }
 function three1() {
-    document.getElementById('picture').src='rpictures/pic31.jpeg'; 
+    document.getElementById('picture').src='rpictures/pic31.JPEG'; 
 }
 function three2() {
-    document.getElementById('picture').src='rpictures/pic32.jpeg'; 
+    document.getElementById('picture').src='rpictures/pic32.JPEG'; 
 }
