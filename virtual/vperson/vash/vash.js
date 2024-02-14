888a257fe4b45826f21ea422696d28b02f65c2b6
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:15:09 -0500

    Add files via upload

---
+++ virtual/vperson/vash/vash.js
@@ -0,0 +1,9 @@
+function one() {
+    document.getElementById('picture').src='../../vpictures/pic2.png';
+}
+function two() {
+    document.getElementById('picture').src='../../vpictures/pic3.png';
+}
+function three() {
+    document.getElementById('picture').src='../../vpictures/pic27.png';
+}
