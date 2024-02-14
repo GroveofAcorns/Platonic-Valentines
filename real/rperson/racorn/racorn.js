4186143ee0cece6d606542ae56aa9335d8a66658
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:07:50 -0500

    Add files via upload

---
+++ real/rperson/racorn/racorn.js
@@ -0,0 +1,36 @@
+function one() {
+    document.getElementById('picture').src='../../rpictures/pic1.JPG';
+}
+function two() {
+    document.getElementById('picture').src='../../rpictures/pic4.JPeG';
+}
+function three() {
+    document.getElementById('picture').src='../../rpictures/pic5.jpeg';
+}
+function four() {
+    document.getElementById('picture').src='../../rpictures/pic13.jpeg'; 
+}
+function five() {
+    document.getElementById('picture').src='../../rpictures/pic14.jpeg';
+}
+function six() {
+    document.getElementById('picture').src='../../rpictures/pic21.jpg';
+}
+function seven() {
+    document.getElementById('picture').src='../../rpictures/pic22.jpg';
+}
+function eight() {
+    document.getElementById('picture').src='../../rpictures/pic23.jpg'; 
+}
+function nine() {
+    document.getElementById('picture').src='../../rpictures/pic24.jpg'; 
+}
+function one0() {
+    document.getElementById('picture').src='../../rpictures/pic25.jpg';
+}
+function one1() {
+    document.getElementById('picture').src='../../rpictures/pic26.jpg';
+}
+function one2() {
+    document.getElementById('picture').src='../../rpictures/pic28.jpg';
+}
