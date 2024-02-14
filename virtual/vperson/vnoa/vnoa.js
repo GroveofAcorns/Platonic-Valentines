d36f7f46547bde05d43be17a020b6da1a745283e
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:15:52 -0500

    Add files via upload

---
+++ virtual/vperson/vnoa/vnoa.js
@@ -0,0 +1,27 @@
+function one() {
+    document.getElementById('picture').src='../../vpictures/pic10.png';
+}
+function two() {
+    document.getElementById('picture').src='../../vpictures/pic16.png';
+}
+function three() {
+    document.getElementById('picture').src='../../vpictures/pic19.png';
+}
+function four() {
+    document.getElementById('picture').src='../../vpictures/pic20.png'; 
+}
+function five() {
+    document.getElementById('picture').src='../../vpictures/pic23.png';
+}
+function six() {
+    document.getElementById('picture').src='../../vpictures/pic24.png';
+}
+function seven() {
+    document.getElementById('picture').src='../../vpictures/pic25.png';
+}
+function eight() {
+    document.getElementById('picture').src='../../vpictures/pic26.png'; 
+}
+function nine() {
+    document.getElementById('picture').src='../../vpictures/pic27.png'; 
+}
