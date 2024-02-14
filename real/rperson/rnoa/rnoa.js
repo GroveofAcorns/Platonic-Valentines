26c91d3d4d8dccb6c9807502149df03993522581
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:10:09 -0500

    Add files via upload

---
+++ real/rperson/rnoa/rnoa.js
@@ -0,0 +1,33 @@
+function one() {
+    document.getElementById('picture').src='../../rpictures/pic5.JPEG';
+}
+function two() {
+    document.getElementById('picture').src='../../rpictures/pic8.JPEG';
+}
+function three() {
+    document.getElementById('picture').src='../../rpictures/pic10.jpeg';
+}
+function four() {
+    document.getElementById('picture').src='../../rpictures/pic11.jpeg'; 
+}
+function five() {
+    document.getElementById('picture').src='../../rpictures/pic12.jpeg';
+}
+function six() {
+    document.getElementById('picture').src='../../rpictures/pic20.jpg';
+}
+function seven() {
+    document.getElementById('picture').src='../../rpictures/pic23.jpg';
+}
+function eight() {
+    document.getElementById('picture').src='../../rpictures/pic24.jpg'; 
+}
+function nine() {
+    document.getElementById('picture').src='../../rpictures/pic25.jpg'; 
+}
+function one0() {
+    document.getElementById('picture').src='../../rpictures/pic27.jpg';
+}
+function one1() {
+    document.getElementById('picture').src='../../rpictures/pic29.jpeg';
+}
