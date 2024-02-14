4fe934cf7debc72062f128f17a0cd845e8caf486
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:09:25 -0500

    Add files via upload

---
+++ real/rperson/rkira/rkira.js
@@ -0,0 +1,18 @@
+function one() {
+    document.getElementById('picture').src='../../rpictures/pic1.JPG';
+}
+function two() {
+    document.getElementById('picture').src='../../rpictures/pic2.JPG';
+}
+function three() {
+    document.getElementById('picture').src='../../rpictures/pic5.jpeg';
+}
+function four() {
+    document.getElementById('picture').src='../../rpictures/pic9.jpg'; 
+}
+function five() {
+    document.getElementById('picture').src='../../rpictures/pic30.jpeg';
+}
+function six() {
+    document.getElementById('picture').src='../../rpictures/pic31.jpeg';
+}
