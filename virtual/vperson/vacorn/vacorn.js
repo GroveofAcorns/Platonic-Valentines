0a85b10e3c4574c377ab9d639df68a4ffb337146
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:14:28 -0500

    Add files via upload

---
+++ virtual/vperson/vacorn/vacorn.js
@@ -0,0 +1,12 @@
+function one() {
+    document.getElementById('picture').src='../../vpictures/pic3.png';
+}
+function two() {
+    document.getElementById('picture').src='../../vpictures/pic4.png';
+}
+function three() {
+    document.getElementById('picture').src='../../vpictures/pic10.png';
+}
+function four() {
+    document.getElementById('picture').src='../../vpictures/pic27.png'; 
+}
