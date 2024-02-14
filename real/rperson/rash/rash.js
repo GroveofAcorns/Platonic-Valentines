98b998d48bbd3980b78faa29d8108c4f5111685f
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:08:44 -0500

    Add files via upload

---
+++ real/rperson/rash/rash.js
@@ -0,0 +1,12 @@
+function one() {
+    document.getElementById('picture').src='../../rpictures/pic5.JPEG';
+}
+function two() {
+    document.getElementById('picture').src='../../rpictures/pic6.JPeG';
+}
+function three() {
+    document.getElementById('picture').src='../../rpictures/pic7.jpeg';
+}
+function four() {
+    document.getElementById('picture').src='../../rpictures/pic32.jpeg'; 
+}
