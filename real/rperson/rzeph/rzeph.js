babbc14527404c483d8a1e7be6bbc9c0474e8d0c
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:10:52 -0500

    Add files via upload

---
+++ real/rperson/rzeph/rzeph.js
@@ -0,0 +1,18 @@
+function one() {
+    document.getElementById('picture').src='../../rpictures/pic5.JPEG';
+}
+function two() {
+    document.getElementById('picture').src='../../rpictures/pic15.JPG';
+}
+function three() {
+    document.getElementById('picture').src='../../rpictures/pic16.jpeg';
+}
+function four() {
+    document.getElementById('picture').src='../../rpictures/pic17.jpg'; 
+}
+function five() {
+    document.getElementById('picture').src='../../rpictures/pic18.jpg';
+}
+function six() {
+    document.getElementById('picture').src='../../rpictures/pic19.jpg';
+}
