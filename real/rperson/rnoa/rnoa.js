7cb19756b7ad2b74cfa0651173382bc0f7115d45
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:36:14 -0500

    Update rnoa.js

--- real/rperson/rnoa/rnoa.js
+++ real/rperson/rnoa/rnoa.js
@@ -5,29 +5,29 @@ function two() {
     document.getElementById('picture').src='../../rpictures/pic8.JPEG';
 }
 function three() {
-    document.getElementById('picture').src='../../rpictures/pic10.jpeg';
+    document.getElementById('picture').src='../../rpictures/pic10.JPEG';
 }
 function four() {
-    document.getElementById('picture').src='../../rpictures/pic11.jpeg'; 
+    document.getElementById('picture').src='../../rpictures/pic11.JPEG'; 
 }
 function five() {
-    document.getElementById('picture').src='../../rpictures/pic12.jpeg';
+    document.getElementById('picture').src='../../rpictures/pic12.JPEG';
 }
 function six() {
-    document.getElementById('picture').src='../../rpictures/pic20.jpg';
+    document.getElementById('picture').src='../../rpictures/pic20.JPG';
 }
 function seven() {
-    document.getElementById('picture').src='../../rpictures/pic23.jpg';
+    document.getElementById('picture').src='../../rpictures/pic23.JPG';
 }
 function eight() {
-    document.getElementById('picture').src='../../rpictures/pic24.jpg'; 
+    document.getElementById('picture').src='../../rpictures/pic24.JPG'; 
 }
 function nine() {
-    document.getElementById('picture').src='../../rpictures/pic25.jpg'; 
+    document.getElementById('picture').src='../../rpictures/pic25.JPG'; 
 }
 function one0() {
-    document.getElementById('picture').src='../../rpictures/pic27.jpg';
+    document.getElementById('picture').src='../../rpictures/pic27.JPG';
 }
 function one1() {
-    document.getElementById('picture').src='../../rpictures/pic29.jpeg';
+    document.getElementById('picture').src='../../rpictures/pic29.JPEG';
 }
