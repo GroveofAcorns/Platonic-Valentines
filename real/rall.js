a3c1c3da1c802e019f3e38c3ef3d801660f87a71
grove <119685016+GroveofAcorns@users.noreply.github.com>
2024-02-14 14:05:01 -0500

    Add files via upload

---
+++ real/rall.js
@@ -0,0 +1,96 @@
+function one() {
+    document.getElementById('picture').src='rpictures/pic1.jpg';
+}
+function two() {
+    document.getElementById('picture').src='rpictures/pic2.jpg';
+}
+function three() {
+    document.getElementById('picture').src='rpictures/pic3.jpeg';
+}
+function four() {
+    document.getElementById('picture').src='rpictures/pic4.jpeg'; 
+}
+function five() {
+    document.getElementById('picture').src='rpictures/pic5.jpeg';
+}
+function six() {
+    document.getElementById('picture').src='rpictures/pic6.jpeg';
+}
+function seven() {
+    document.getElementById('picture').src='rpictures/pic7.jpeg';
+}
+function eight() {
+    document.getElementById('picture').src='rpictures/pic8.jpeg'; 
+}
+function nine() {
+    document.getElementById('picture').src='rpictures/pic9.jpg'; 
+}
+function one0() {
+    document.getElementById('picture').src='rpictures/pic10.jpeg';
+}
+function one1() {
+    document.getElementById('picture').src='rpictures/pic11.jpeg';
+}
+function one2() {
+    document.getElementById('picture').src='rpictures/pic12.jpeg';
+}
+function one3() {
+    document.getElementById('picture').src='rpictures/pic13.jpeg'; 
+}
+function one4() {
+    document.getElementById('picture').src='rpictures/pic14.jpeg';
+}
+function one5() {
+    document.getElementById('picture').src='rpictures/pic15.jpg';
+}
+function one6() {
+    document.getElementById('picture').src='rpictures/pic16.jpeg';
+}
+function one7() {
+    document.getElementById('picture').src='rpictures/pic17.jpg'; 
+}
+function one8() {
+    document.getElementById('picture').src='rpictures/pic18.jpg'; 
+}
+function one9() {
+    document.getElementById('picture').src='rpictures/pic19.jpg';
+}
+function two0() {
+    document.getElementById('picture').src='rpictures/pic20.jpg';
+}
+function two1() {
+    document.getElementById('picture').src='rpictures/pic21.jpg';
+}
+function two2() {
+    document.getElementById('picture').src='rpictures/pic22.jpg'; 
+}
+function two3() {
+    document.getElementById('picture').src='rpictures/pic23.jpg';
+}
+function two4() {
+    document.getElementById('picture').src='rpictures/pic24.jpg';
+}
+function two5() {
+    document.getElementById('picture').src='rpictures/pic25.jpg';
+}
+function two6() {
+    document.getElementById('picture').src='rpictures/pic26.jpg'; 
+}
+function two7() {
+    document.getElementById('picture').src='rpictures/pic27.jpg'; 
+}
+function two8() {
+    document.getElementById('picture').src='rpictures/pic28.jpg';
+}
+function two9() {
+    document.getElementById('picture').src='rpictures/pic29.jpeg';
+}
+function three0() {
+    document.getElementById('picture').src='rpictures/pic30.jpeg'; 
+}
+function three1() {
+    document.getElementById('picture').src='rpictures/pic31.jpeg'; 
+}
+function three2() {
+    document.getElementById('picture').src='rpictures/pic32.jpeg'; 
+}
